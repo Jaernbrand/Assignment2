17bb81b97776f1270ab7424815c201a724f5bc27
Jaernbrand <henrikjarnbrand@gmail.com>
2014-12-09 14:16:59 +0100

    YES!!

--- safe.clj
+++ safe.clj
@@ -1,7 +1,13 @@
 (defmacro safe [variable form]
-	`(variable )
+	;`(let [locVar# variable] 
+	`(let ~variable
+		(if (instance? java.io.Closeable (get ~variable 0))
+			~form
+			; Exception!!!!
+		)
+	)
 )
 
 ; Exemplet från uppgiftsbeskrivningen.
-(import java.io.FileReader java.io.File)
-(safe [s (new FileReader (new File "file.txt"))] (.read s))
+;(import java.io.FileReader java.io.File)
+;(safe [s (new FileReader (new File "file.txt"))] (.read s))
