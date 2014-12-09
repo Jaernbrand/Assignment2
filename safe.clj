84ed7c0da5ff2a8245b62e5b48d9888b2361b5a1
Jaernbrand <henrikjarnbrand@gmail.com>
2014-12-09 11:48:33 +0100

    Inte halvfördigt skelett längre

--- safe.clj
+++ safe.clj
@@ -1,5 +1,7 @@
 (defmacro safe [variable form]
-	)
+	`(variable )
+)
 
+; Exemplet från uppgiftsbeskrivningen.
 (import java.io.FileReader java.io.File)
-(safe [s (new FileReader (new File))])
+(safe [s (new FileReader (new File "file.txt"))] (.read s))
