2bd093810ceef6784277313f4b9520cf09300d3f
Jaernbrand <henrikjarnbrand@gmail.com>
2014-12-10 13:21:15 +0100

    extractRowValues FUNGERAR!! :D

--- sql-like.clj
+++ sql-like.clj
@@ -1,3 +1,36 @@
-(defmacro select [key from &args]
-
+(defmacro from [table]
+	`(if (set? ~table)
+		(do ~table)
+		(throw (Exception. "Table is not a set!"))
+	)
 )
+
+(defn extractRowValues [row keys]
+	(let [extRow {}] 
+		(if ( > (count keys) 1)
+			(conj
+				(assoc extRow (first keys) (get row (first keys)))	
+				(conj extRow (extractRowValues row (rest keys)))
+			)
+			
+			(assoc extRow (first keys) (get row (first keys)))	
+		)
+	)	
+)
+
+(defn extractColumns [table & keys]
+	(let [retTable# #{}]
+		(conj retTable# (extractRowValues (first table) keys))
+		(if ( > (count table) 1) 
+			(extractColumns (rest table))
+		)
+	retTable#
+	)
+)
+
+(defmacro select [keys from table & args]
+	`(let [t# (~from ~table)]
+		(extractColumns t#, keys)
+	)
+)
+
